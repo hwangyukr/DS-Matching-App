7c74234ca54866baa129701e534af1eef3cee576
LEEJ0NGWAN <lastffx@gmail.com>
2020-06-11 02:07:41 +0900

    board repository: 게시물 id로 특정 게시물 가져오기

--- src/main/java/Board/BoardRepository.java
+++ src/main/java/Board/BoardRepository.java
@@ -1,6 +1,8 @@
 package Board;
 
+import Common.DBManager;
 import Common.Result;
+import Team.Team;
 import User.User;
 
 import java.sql.ResultSet;
@@ -11,8 +13,8 @@ import java.util.List;
 import kr.ac.konkuk.ccslab.cm.info.CMInfo;
 import kr.ac.konkuk.ccslab.cm.manager.CMDBManager;
 
-public class BoardRepository extends CMDBManager {
-    public List<Board> getAllBoards(Long teamId, Result result, CMInfo cmInfo) {
+public class BoardRepository {
+	public List<Board> getAllBoards(Long teamId, Result result, CMInfo cmInfo) {
     	String query = 
     			"select b.board_id, b.title, b.content, u.user_id, u.user_name" +
     			"from user u, board b" +
@@ -50,6 +52,46 @@ public class BoardRepository extends CMDBManager {
         return boards;
     }
 
+    public Board getBoardById(Long boardId, Result result, CMInfo cmInfo) {
+    	String query =
+    			"select b.title, b.content, u.user_id, u.user_name, t.team_id, t.team_name" +
+    			"from board b, user u, team t" +
+    			"where b.board_id = '" + boardId + "' and b.author_id = u.user_id and b.team_id = t.team_id";
+    	ResultSet res = CMDBManager.sendSelectQuery(query, cmInfo);
+    	Board board = null;
+		try {
+			if (res.first()) {
+				Long user_id = res.getLong("user_id");
+				Long team_id = res.getLong("team_id");
+				String user_name = res.getString("user_name");
+				String team_name = res.getString("team_name");
+				String title = res.getString("title");
+				String content = res.getString("content");
+				User user = new User.Builder()
+						.id(user_id)
+						.name(user_name)
+						.build();
+				Team team = new Team.Builder()
+						.id(team_id)
+						.name(team_name)
+						.build();
+				board = new Board.Builder()
+						.id(boardId)
+						.user(user)
+						.team(team)
+						.title(title)
+						.content(content)
+						.build();
+			}
+		} catch (SQLException e) {
+			result.setMsg("실패하였습니다");
+			result.setSuccess(false);
+		}
+        result.setMsg("성공하였습니다");
+        result.setSuccess(true);
+    	return board;
+    }
+    
     public Board save(Board board, CMInfo cmInfo) {
         return null;
     }
