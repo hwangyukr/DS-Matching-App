beb57b4e153b29c2023b7c99e7b2bef6cfcc1dca
LEEJ0NGWAN <lastffx@gmail.com>
2020-06-12 17:13:32 +0900

    profile handler: 프로필 수정

--- src/main/java/User/ProfileHandler.java
+++ src/main/java/User/ProfileHandler.java
@@ -23,7 +23,7 @@ public class ProfileHandler {
     
 	public TokenProvider.TokenResult getUserInfo(CMUserEvent ue) {
 	    String token = ue.getEventField(CMInfo.CM_STR, "token");
-	    if(token == null){
+	    if (token == null){
 	        ue.setEventField(CMInfo.CM_INT, "success", "0");
 	        ue.setEventField(CMInfo.CM_STR, "msg", "NOT AUTHORIZED");
 	        cmServerStub.send(ue, ue.getSender());
@@ -44,14 +44,14 @@ public class ProfileHandler {
         if(validResult == null) return;
         
         Long userId = Long.valueOf(ue.getEventField(CMInfo.CM_LONG, "user_id"));
-        if(userId == null) {
+        if (userId == null) {
         	userId = validResult.getId();
         }
         
         Result result = new Result();
         Profile profile = profileService.getProfile(userId, result);
         
-        if(!result.isSuccess()) {
+        if (!result.isSuccess()) {
             handleError(result, ue);
             return;
         }
@@ -70,7 +70,7 @@ public class ProfileHandler {
 	public void postProfile(CMUserEvent ue) {
         ue.setStringID("POST-PROFILE-REPLY");
         TokenProvider.TokenResult validResult = getUserInfo(ue);
-        if(validResult == null) return;
+        if (validResult == null) return;
         
         Role role;
         String roleName = ue.getEventField(CMInfo.CM_STR, "role");
@@ -89,7 +89,7 @@ public class ProfileHandler {
         Result result = new Result();
         Profile profile = profileService.postProfile(validResult, result, role, content, photo, portforlio);
         
-        if(!result.isSuccess()) {
+        if (!result.isSuccess()) {
             handleError(result, ue);
             return;
         }
@@ -104,4 +104,70 @@ public class ProfileHandler {
             e.printStackTrace();
         }
 	}
+	
+	public void putProfile(CMUserEvent ue) {
+        ue.setStringID("PUT-PROFILE-REPLY");
+        TokenProvider.TokenResult validResult = getUserInfo(ue);
+        if(validResult == null) return;
+        
+        Long userId = Long.valueOf(ue.getEventField(CMInfo.CM_LONG, "user_id"));
+        
+        if (userId == null) {
+        	userId = validResult.getId();
+        }
+        
+        Role role;
+        String roleName = ue.getEventField(CMInfo.CM_STR, "role");
+        String content = ue.getEventField(CMInfo.CM_STR, "content");
+        String photo = ue.getEventField(CMInfo.CM_STR, "photo");
+        String portforlio = ue.getEventField(CMInfo.CM_STR, "portforlio");
+        
+        Result result = new Result();
+        Profile profile = profileService.getProfile(userId, result);
+        
+        if (!result.isSuccess()) {
+            handleError(result, ue);
+            return;
+        }
+        
+        if (roleName == null) {
+        	role = profile.getRole();
+        }
+        else {
+            try {
+            	role = Role.valueOf(roleName);
+            }
+            catch (java.lang.IllegalArgumentException e) {
+            	handleError(new Result("입력값을 확인하세요", false), ue);
+            	return;
+            }
+        }
+        
+//        TODO: 과연 텅빈 값으로 오는 요청이 의도하지 않은 값이라고 할 수 있는가?
+//       정말로 프사를 내리거나 포트폴리오를 지우거나, 자기소개를 빈칸으로 놔두는 경우에 대해서는 어떻게 설명할 것인가?
+//        if (content == null) {
+//        	content = profile.getContent();
+//        }
+//        
+//        if (photo == null) {
+//        	photo = profile.getPhoto();
+//        }
+//        
+//        if (portforlio == null) {
+//        	portforlio = profile.getPortforlio();
+//        }
+        
+        Long id = profileService.putProfile(profile, result, role, content, photo, portforlio);
+        if(!result.isSuccess()) {
+            handleError(result, ue);
+            return;
+        }
+        
+        ue.setEventField(CMInfo.CM_INT,"success", "1");
+        ue.setEventField(CMInfo.CM_STR, "msg", "성공하였습니다");
+        ue.setEventField(CMInfo.CM_STR, "profile", String.valueOf(id));
+        cmServerStub.send(ue, ue.getSender());
+	}
+	
+	
 }
