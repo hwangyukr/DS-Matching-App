13313b40df94c295638e75419bb7cbe3e539371c
LEEJ0NGWAN <lastffx@gmail.com>
2020-06-12 14:37:36 +0900

    profile: 필드 정의 및 setter와 getter 그리고 builder

--- src/main/java/User/Profile.java
+++ src/main/java/User/Profile.java
@@ -1,6 +1,122 @@
 package User;
 
+import Team.Role;
+
 public class Profile {
     private Long id;
     private User user;
+    private Role role;
+    private String content;
+    private String photo;
+    private String portforlio;
+    
+    public void setId(Long id) {
+    	this.id = id;
+    }
+    
+    public void setUser(User user) {
+    	this.user = user;
+    }
+    
+    public void setRole(Role role) {
+    	this.role = role;
+    }
+    
+    public void setContent(String content) {
+    	this.content = content;
+    }
+    
+    public void setPhoto(String photo) {
+    	this.photo = photo;
+    }
+    
+    public void setPortforlio(String portforlio) {
+    	this.portforlio = portforlio;
+    }
+    
+    public Long getId() {
+    	return id;
+    }
+    
+    public User getUser() {
+    	return user;
+    }
+    
+    public Role getRole() {
+    	return role;
+    }
+    
+    public String getContent() {
+    	return content;
+    }
+    
+    public String getPhoto() {
+    	return photo;
+    }
+    
+    public String getPortforlio() {
+    	return portforlio;
+    }
+    
+    public Profile() {
+    	
+    }
+    
+    public static class Builder {
+        private Long id;
+        private User user;
+        private Role role;
+        private String content;
+        private String photo;
+        private String portforlio;
+        
+        public Builder() {
+        }
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+        
+        public Builder user(User user) {
+        	this.user = user;
+        	return this;
+        	
+        }
+        
+        public Builder role(Role role) {
+        	this.role = role;
+        	return this;
+        }
+        
+        public Builder content(String content) {
+        	this.content = content;
+        	return this;
+        }
+        
+        public Builder photo(String photo) {
+        	this.photo = photo;
+        	return this;
+        }
+        
+        public Builder portforlio(String portforlio) {
+        	this.portforlio = portforlio;
+        	return this;
+        }
+
+        public Profile build() {
+            return new Profile(this);
+        }
+
+
+    }
+    
+    public Profile(Builder builder) {
+        this.id = builder.id;
+        this.user = builder.user;
+        this.role = builder.role;
+        this.content = builder.content;
+        this.photo = builder.photo;
+        this.portforlio = builder.portforlio;
+    }
 }
